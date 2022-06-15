8f2d395cb8299660cb56ba993b4406a48dc45fc7
Carlos <carlos.av.amorim@gmail.com>
2022-06-15 15:34:02 -0300

    style: Creating initial style os Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { Container, Header, Icon, Photo, User, UserGreeting, UserInfo, UserName, UserWrapper } from "./styles";
 
 export const Dashboard = () => {
   return (
     <Container>
-      <Title>
-        Carlos Amorim
-      </Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/83739628?v=4'}} />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Carlos</UserName>
+            </User>
+
+          </UserInfo>
+
+          <Icon name="power" />
+
+        </UserWrapper>
+
+      </Header>
+      
     </Container>
   )
 }
