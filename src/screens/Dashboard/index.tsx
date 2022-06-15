7bcb7d70676bcd0874f09f67cf74d65df39dd3e2
Carlos <carlos.av.amorim@gmail.com>
2022-06-15 13:45:38 -0300

    style: Create types fos theme context and themeprovider

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <Title>
+        Carlos Amorim
+      </Title>
+    </Container>
+  )
+}
